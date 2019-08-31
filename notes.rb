ac9344030f308f04c31d8cfcb47c26dbc53df7c7
Jennifer Nicely <pipeline38@hotmail.com>
2019-08-31 16:09:06 +0000

    Automatically backed up by Learn

--- notes.rb
+++ notes.rb
@@ -1,2 +1,11 @@
 def find_element_index(array, value_to_find)
-  array
+  array.index(value_to_find)
+end
+
+def find_max_value(array)
+  array.max_value
+end
+
+def find_min_value(array)
+  array.min_value
+end
